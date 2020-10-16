d84ae11d98a50a14fa90acb021889d4bdfdd02b7
Jacob M. Litman <jacob.m.litman@gmail.com>
2020-10-16 11:56:37 -0500

    Moved in previously separate SubPots.py; beginning initial EspAnalysis.py testing.

--- EspFitting/EspAnalysis.py
+++ EspFitting/EspAnalysis.py
@@ -4,51 +4,65 @@ import argparse
 import os
 import subprocess
 from JMLUtils import eprint, verbose_call
-
-#def check_probedir(d: os.DirEntry) -> bool:
-#    if d.is_dir():
+import SubPots
 
 
 def main_inner(tinker_path: str = '', gauss_path: str = ''):
+    assert tinker_path is not None and gauss_path is not None
     probe_dirs = [f.path for f in os.scandir(".") if (f.is_dir() and os.path.exists(f"{f.path}{os.sep}QM_PR.xyz"))]
 
-    if not tinker_path.endswith(os.sep):
+    if not tinker_path.endswith(os.sep) and tinker_path != '':
         tinker_path += os.sep
     potential = tinker_path + "potential"
 
-    if not gauss_path.endswith(os.sep):
+    if not gauss_path.endswith(os.sep) and gauss_path != '':
         gauss_path += os.sep
     formchk = gauss_path + "formchk"
     cubegen = gauss_path + "cubegen"
 
     for pdir in probe_dirs:
         os.chdir(pdir)
-        verbose_call([potential, "1", "QM_PR.xyz"])
+        eprint(f"Operating in directory {pdir}\n")
+        verbose_call([potential, "1", "QM_PR.xyz", 'QM_PR.key'])
         verbose_call([formchk, "QM_PR.chk"])
+
         # TODO: Check that it actually is MP2 potential.
         with open('QM_PR.grid', 'r') as f:
             #eprint(f"Calling {cubegen} 0 potential=MP2 QM_PR.fchk QM_PR.cube -5 h < MM_PR.grid")
             #subprocess.run([cubegen, '0', 'potential=MP2', 'QM_PR.fchk', 'QM_PR.cube', '-5', 'h'], stdin=f)
             verbose_call([cubegen, '0', 'potential=MP2', 'QM_PR.fchk', 'QM_PR.cube', '-5', 'h'], kwargs={'stdin': f})
+
         verbose_call([potential, "2", "QM_PR.cube"])
         verbose_call([potential, "3", "PR_NREF.xyz", "Y"])
-        # TODO: Bring in AddPots.py: AddPots.py "${PR_NREF}.pot" "${QM_PR}.pot" > "${QM_PR_BACK}.pot"
+
+        with open('QM_PR_BACK.pot', 'w') as f:
+            with open('QM_PR_BACK.potlog', 'w') as f2:
+                SubPots.main_inner('PR_NREF.pot', 'QM_PR.pot', out=f, err=f2, subtract=False)
+
         with open('unfit_diff.log', 'w') as f:
             verbose_call([potential, "5", "MM_PR.xyz", "QM_PR_BACK.pot", "Y"], kwargs={'stdout': f})
+
         with open('QM_PR.grid', 'r') as f:
             verbose_call([cubegen, '0', 'potential=MP2', 'QM_REF.fchk', 'QM_REF.cube', '-5', 'h'], kwargs={'stdin': f})
+
         verbose_call([potential, '2', 'QM_REF.cube'])
-        # TODO: Bring in SubPots.py: SubPots.py "${QM_PR}.pot" "${QM_REF}.pot" X Y Z Radius 1> qm_polarization.pot 2> polarize_diff_qm.log
-        
+        SubPots.main_inner('QM_PR.pot', 'QM_REF.pot')
+        with open('qm_polarization.pot', 'w') as f:
+            with open('polarize_diff_qm.log', 'w') as f2:
+                # TODO: Log atom-focal information
+                SubPots.main_inner('QM_PR.pot', 'QM_REF.pot', out=f, err=f2, subtract=True)
+        eprint('\n')
         os.chdir("..")
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', dest='probe_type', type=int, default=999, help='Probe aotm type')
-    parser.add_argument('-t', dest='tinker_path', type=str, default=None, help='Path to Tinker executables')
-    parser.add_argument('-g', dest='gauss_path', type=str, default=None, help='Path to Gaussian executables')
+    parser.add_argument('-t', dest='tinker_path', type=str, default='', help='Path to Tinker executables')
+    parser.add_argument('-g', dest='gauss_path', type=str, default='', help='Path to Gaussian executables')
     args = parser.parse_args()
-    main_inner(tinker_path=args.tinker_path)
+    main_inner(tinker_path=args.tinker_path, gauss_path=args.gauss_path)
+
 
 if __name__ == "__main__":
     main()
---
+++ EspFitting/SubPots.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import math
+import numpy as np
+import io
+from JMLUtils import eprint
+
+DEFAULT_RADIUS = 3.0
+
+
+def main():
+    if not sys.argv or len(sys.argv) < 3:
+        # TODO: Restore effective scriptiness (i.e. all arguments).
+        eprint("Must have two arguments (potential files to add together).\n")
+        sys.exit(1)
+    main_inner(sys.argv[1], sys.argv[2])
+
+
+def main_inner(f1: str, f2: str, out: io.TextIOWrapper = sys.stdout, err: io.TextIOWrapper = sys.stderr, 
+               subtract: bool = False, x: float = None, y: float = None, z: float = None, 
+               radius: float = DEFAULT_RADIUS):
+    all_diffs = []
+    do_focus = False
+    focus_xyz = (math.nan, math.nan, math.nan)
+    focus_r2 = math.nan
+    focus_diffs = []
+    if x is not None:
+        if subtract:
+            focus_xyz = (x, y, z)
+            focus_r2 = radius * radius
+            do_focus = True
+        else:
+            err.write("Focus atom (region of special interest difference) is disabled for adding .pot files!\n")
+
+    if subtract:
+        err.write(f"Subtracting potential {f2} from {f1}\n")
+    else:
+        err.write(f"Adding potential {f2} to {f1}\n")
+    keep_patt = re.compile(r"(.+ )(-?[0-9]+\.[0-9]+) *$")
+    with open(f1, "r") as p1:
+        with open(f2, "r") as psub:
+            p1_line = p1.readline()
+            psub_line = psub.readline()
+            n_grid_points = int(p1_line.strip().split()[0])
+            assert n_grid_points == int(psub_line.strip().split()[0])
+            print(p1_line, end='')
+            for i in range(n_grid_points):
+                p1_line = p1.readline()
+                p1_match = keep_patt.search(p1_line)
+                psub_line = psub.readline()
+                psub_match = keep_patt.search(psub_line)
+
+                pot = float(p1_match.group(2))
+                sub_pot = float(psub_match.group(2))
+                if subtract:
+                    this_diff = pot - sub_pot
+                else:
+                    this_diff = pot + sub_pot
+                all_diffs.append(this_diff)
+                out.write(f"{p1_match.group(1)}{this_diff:.6f}\n")
+                if do_focus:
+                    toks = p1_match.group(1).strip().split()
+                    diff_xyz = (float(toks[1]) - focus_xyz[0], float(toks[2]) - focus_xyz[1], float(toks[3]) - focus_xyz[2])
+                    dist2 = 0
+                    for dx in diff_xyz:
+                        dist2 += (dx * dx)
+                    if dist2 <= focus_r2:
+                        focus_diffs.append(this_diff)
+                # TODO: Assertion that psub_line otherwise patches p1_line
+
+    if subtract:
+        all_arr = np.array(all_diffs)
+        rms_all = math.sqrt(np.mean(np.square(all_arr)))
+        mue_all = np.mean(np.abs(all_arr))
+        mse_all = np.mean(all_arr)
+        sd_all = np.std(all_arr, ddof=1)
+
+        err.write(f"Statistics over all {len(all_diffs):d} grid points: RMSD {rms_all:.6f}, MUE {mue_all:.6f}, MSE {mse_all:.6f}, SD {sd_all:.6f}\n")
+        if do_focus:
+            all_arr = np.array(focus_diffs)
+            rms_all = math.sqrt(np.mean(np.square(all_arr)))
+            mue_all = np.mean(np.abs(all_arr))
+            mse_all = np.mean(all_arr)
+            sd_all = np.std(all_arr, ddof=1)
+            eprint(f"Statistics over {len(focus_diffs):d} points in region of interest: RMSD {rms_all:.6f}, MUE {mue_all:.6f}, MSE {mse_all:.6f}, "
+                   f"SD {sd_all:.6f}")
+
+
+if __name__ == "__main__":
+    main()
