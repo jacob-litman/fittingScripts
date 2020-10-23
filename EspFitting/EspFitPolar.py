415dc1137668fc966e52e5fe76af050d5bd47dda
Jacob M. Litman <jacob.m.litman@gmail.com>
2020-10-23 12:31:49 -0500

    Enhancements to EspFitPolar.py with several new options. Use of a minimization strategy other than least_squares does not currently appear to be successful.

--- EspFitting/EspFitPolar.py
+++ EspFitting/EspFitPolar.py
@@ -2,14 +2,16 @@
 # General structure: subtract out unfit ESP (multipoles only) from each fit. Then, use MM probe & match ESP changes
 
 import argparse
-import numpy as np
 import os
 import re
 import sys
-from StructureXYZ import StructXYZ
-from JMLUtils import eprint, version_file
 from typing import Sequence
+
+import numpy as np
 import scipy.optimize
+
+from JMLUtils import eprint, version_file
+from StructureXYZ import StructXYZ
 
 polar_patt = re.compile(r"^(polarize +)(\d+)( +)(\d+\.\d+)( +.+)$")
 
@@ -17,7 +19,7 @@ polar_patt = re.compile(r"^(polarize +)(\d+)( +)(\d+\.\d+)( +.+)$")
 def read_initial_polarize(from_fi: str, probe_types: Sequence[int] = None) -> np.ndarray:
     if probe_types is None:
         probe_types = [999]
-    #polar_types = []  # Would contain the atom types; not necessary yet.
+    # polar_types = []  # Would contain the atom types; not necessary yet.
     polarizabilities = []
     with open(from_fi, 'r') as r:
         for line in r:
@@ -26,7 +28,7 @@ def read_initial_polarize(from_fi: str, probe_types: Sequence[int] = None) -> np
                 assert m
                 atype = int(m.group(2))
                 if atype not in probe_types:
-                    #polar_types.append(atype)
+                    # polar_types.append(atype)
                     polarizabilities.append(float(m.group(4)))
     return np.array(polarizabilities)
 
@@ -62,40 +64,65 @@ def edit_keyf_polarize(x: np.ndarray, from_fi: str, to_file: str, probe_types: S
                     w.write(line)
 
 
-def cost_fun(x: np.ndarray, qm_diffs: Sequence[np.ndarray], mm_refs: Sequence[np.ndarray],
-             probe_files: Sequence[StructXYZ], to_files: Sequence[str], key_files: Sequence[str] = None,
-             potential: str = 'potential', mol_pols: np.ndarray = None, wt_molpols: float = 0.0) -> np.ndarray:
+def cost_fun_residual(x: np.ndarray, qm_diffs: Sequence[np.ndarray], mm_refs: Sequence[np.ndarray],
+                      probe_files: Sequence[StructXYZ], to_files: Sequence[str], key_files: Sequence[str] = None,
+                      potential: str = 'potential', mol_pols: np.ndarray = None, wt_molpols: float = 0.0) -> np.ndarray:
     n_probe = len(qm_diffs)
     assert n_probe == len(mm_refs) and n_probe == len(probe_files)
     if key_files is None:
         key_files = [pf.key_file for pf in probe_files]
 
-    #tot_sq_diff = 0
     residuals = []
     for i in range(n_probe):
         edit_keyf_polarize(x, key_files[i], to_files[i])
         mm_diff = probe_files[i].get_esp(potential=potential, keyf=to_files[i])
-        mm_diff[:,3] -= mm_refs[i][:,3]
+        mm_diff[:, 3] -= mm_refs[i][:, 3]
         # Assert that coordinates are equivalent. Can substitute in numpy.all_close
-        assert np.array_equal(qm_diffs[i][:,0:2], mm_diff[:,0:2])
-        qm_mm_diff = qm_diffs[i][:,3] - mm_diff[:,3]
+        assert np.array_equal(qm_diffs[i][:, 0:2], mm_diff[:, 0:2])
+        qm_mm_diff = qm_diffs[i][:, 3] - mm_diff[:, 3]
         residuals.append(qm_mm_diff)
-        # Use mean rather than sum so as to normalize across different grid sizes.
-        # TODO: Use same grid for everything, and/or focal-point weighting.
-        #tot_sq_diff += np.mean(np.square(qm_mm_diff))
     sys.stderr.flush()
     sys.stdout.flush()
     return np.concatenate(residuals)
-    #return tot_sq_diff
 
 
-def main_inner(tinker_path: str = '', probe_types: Sequence[int] = None):
+def cost_fun(x: np.ndarray, qm_diffs: Sequence[np.ndarray], mm_refs: Sequence[np.ndarray],
+             probe_files: Sequence[StructXYZ], to_files: Sequence[str], key_files: Sequence[str] = None,
+             potential: str = 'potential', mol_pols: np.ndarray = None, wt_molpols: float = 0.0) -> float:
+    n_probe = len(qm_diffs)
+    assert n_probe == len(mm_refs) and n_probe == len(probe_files)
+    if key_files is None:
+        key_files = [pf.key_file for pf in probe_files]
+
+    tot_sq_diff = 0
+    for i in range(n_probe):
+        edit_keyf_polarize(x, key_files[i], to_files[i])
+        mm_diff = probe_files[i].get_esp(potential=potential, keyf=to_files[i])
+        mm_diff[:, 3] -= mm_refs[i][:, 3]
+        # Assert that coordinates are equivalent. Can substitute in numpy.all_close
+        assert np.array_equal(qm_diffs[i][:, 0:2], mm_diff[:, 0:2])
+        qm_mm_diff = qm_diffs[i][:, 3] - mm_diff[:, 3]
+        # Use mean rather than sum so as to normalize across different grid sizes.
+        # TODO: Use same grid for everything, and/or focal-point weighting.
+        tot_sq_diff += np.mean(np.square(qm_mm_diff))
+    sys.stderr.flush()
+    sys.stdout.flush()
+    return tot_sq_diff
+
+
+def main_inner(tinker_path: str = '', probe_types: Sequence[int] = None, tol: float = None, x: Sequence[float] = None,
+               min_strat: str = 'least_squares', residual: bool = False, maxiter: int = 250):
     assert tinker_path is not None
     if probe_types is None:
         probe_types = [999]
-    # I will be fitting to <atom><ID>/qm_polarization.pot
-    # Each round, I need to take QM_PR.xyz w/ modified key, get its potential, and subtract the probe potential.
-    # Question: fit to every single grid point, or just RMS for that probe placement?
+
+    least_sq = (min_strat.lower() == 'least_squares') or (min_strat.lower() == 'ls')
+
+    if tol is None:
+        if least_sq:
+            tol = 1E-5
+        else:
+            tol = 1E-6
 
     probe_dirs = [f.path for f in os.scandir(".") if (f.is_dir() and os.path.exists(f"{f.path}{os.sep}QM_PR.xyz"))]
 
@@ -119,23 +146,50 @@ def main_inner(tinker_path: str = '', probe_types: Sequence[int] = None):
     key_files = [pf.key_file for pf in structures]
     out_keys = [version_file(kf) for kf in key_files]
 
-    x = read_initial_polarize(structures[0].key_file, probe_types=probe_types)
-    opt_args = (probe_diffs, mm_refs, structures, out_keys, key_files, potential, None, 0.0)
-    verbosity = 2
-    bounds = (0, np.inf)
+    if x is None:
+        x = read_initial_polarize(structures[0].key_file, probe_types=probe_types)
+
+    cost_fun_args = (probe_diffs, mm_refs, structures, out_keys, key_files, potential, None, 0.0)
     eprint("Read input polarizabilities: beginning optimization.")
-    ls_result = scipy.optimize.least_squares(cost_fun, x, jac='3-point', args=opt_args, verbose=verbosity,
-                                             bounds=bounds)
-    eprint(f'\n\nLeast squares result:\n{ls_result}')
+    if least_sq:
+        bounds = (0, np.inf)
+        if residual:
+            func = cost_fun_residual
+        else:
+            func = cost_fun
+        ls_result = scipy.optimize.least_squares(func, x, jac='3-point', args=cost_fun_args, verbose=2,
+                                                 bounds=bounds, xtol=tol)
+        eprint(f'\n\nLeast squares result:\n{ls_result}')
+    else:
+        bounds = [(0, None) for xi in x]
+        opt_args = {'disp': 1, 'maxiter': maxiter, 'gtol': tol, 'maxcor': 25}
+        # TODO: Not hardcode in L-BFGS-B.
+        min_result = scipy.optimize.minimize(cost_fun, x, args=cost_fun_args, options=opt_args, method='L-BFGS-B',
+                                             jac='3-point', bounds=bounds)
+        eprint(f'\n\nMinimization result:\n{min_result}')
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', dest='tinker_path', type=str, default='', help='Path to Tinker executables')
-    parser.add_argument('-p', dest='probe_type', type=int, default=999, help='Probe atom type')
+    parser.add_argument('-t', '--tinkerpath', dest='tinker_path', type=str, default='', help='Path to Tinker '
+                                                                                             'executables')
+    parser.add_argument('-p', '--probetype', dest='probe_type', type=int, default=999, help='Probe atom type')
+    parser.add_argument('-e', '--tol', dest='tol', type=float, default=None,
+                        help='Cease optimization at this tolerance (otherwise algorithm-dependent default)')
+    # parser.add_argument('-x', type=Sequence[float], default=None, help='Manually specify starting polarizabilities')
+    parser.add_argument('-m', '--minstrategy', dest='minstrategy', default='least_squares', type=str,
+                        help='Choose optimization method from least_squares or BFGS')
+    parser.add_argument('-r', '--residual', dest='residual', action='store_true',
+                        help='Use residual rather than the target function for least-squares optimization '
+                             '(least_squares only!)')
+    parser.add_argument('-i', '--maxiter', dest='max_iter', type=int, default=250, help='Maximum iterations of the '
+                                                                                        'optimizer')
+
     args = parser.parse_args()
-    
-    main_inner(tinker_path=args.tinker_path, probe_types=[args.probe_type])
+
+    main_inner(tinker_path=args.tinker_path, probe_types=[args.probe_type], tol=args.tol, min_strat=args.minstrategy,
+               residual=args.residual, maxiter=args.max_iter)
+
 
 if __name__ == "__main__":
     main()
